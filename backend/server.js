59a4c2e55093765f8cc84cef0c6125dc928ced23
Ibrahim Bilal <ibrahim.05.bilal@gmail.com>
2025-07-09 11:01:02 -0400

    fixed prisma integration and added test post route

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,38 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// Example route
-app.get('/movies', (req, res) => {
-  res.json([
-    { id: 1, title: "Inception" },
-    { id: 2, title: "Interstellar" },
-    { id: 3, title: "The Matrix" }
-  ]);
+const { PrismaClient } = require('./generated/prisma');
+const prisma = new PrismaClient();
+
+app.use(express.json());
+
+app.get('/movies', async (req, res) => {
+  try {
+    const movies = await prisma.movie.findMany();
+    res.json(movies);
+  } catch (error) {
+    console.error('GET /movies error:', error);
+    res.status(500).json({ error: 'Something went wrong', details: error.message });
+  }
+});
+
+
+app.post('/movies', async (req, res) => {
+  const { title, genre } = req.body;
+
+  try {
+    const newMovie = await prisma.movie.create({
+      data: {
+        title,
+        genre
+      }
+    });
+
+    res.status(201).json(newMovie);
+  } catch (error) {
+    console.error('POST /movies error:', error);
+    res.status(500).json({ error: 'Something went wrong', details: error.message });
+  }
 });
 
 app.listen(PORT, () => {
